0effcf6ea34f181be1fc123f250a27027c76787f
Pavel Kambalin <16853211+kambalin@users.noreply.github.com>
2023-06-19 19:38:11 +0300

    * EntityDataQueryBase added CreatedQueryParams & ModifiedQueryParams * EntityDataQueryBase removed FromModifiedBy

---
+++ EFDM.Core/DataQueries/DatePeriodQueryParams.cs
@@ -0,0 +1,18 @@
+﻿using EFDM.Abstractions.DataQueries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace EFDM.Core.DataQueries {
+
+    public class DatePeriodQueryParams {
+
+        #region fields & properties
+
+        public DateTime? LessOrEquals { get; set; }
+        public DateTime? MoreOrEquals { get; set; }
+
+        #endregion fields & properties
+    }
+}
--- EFDM.Core/DataQueries/EntityDataQueryBase.cs
+++ EFDM.Core/DataQueries/EntityDataQueryBase.cs
@@ -11,7 +11,8 @@ namespace EFDM.Core.DataQueries {
 
         public int[] CreatedByIds { get; set; }
         public int[] ModifiedByIds { get; set; }
-        public DateTimeOffset? FromModifiedBy { get; set; }
+        public DatePeriodQueryParams CreatedQueryParams { get; set; } = new DatePeriodQueryParams();
+        public DatePeriodQueryParams ModifiedQueryParams { get; set; } = new DatePeriodQueryParams();
 
         public override IQueryFilter<TModel> ToFilter() {
             var and = new QueryFilter<TModel>();
@@ -22,8 +23,17 @@ namespace EFDM.Core.DataQueries {
             if (ModifiedByIds?.Any() == true)
                 and.Add(x => ModifiedByIds.Contains(x.ModifiedById));
 
-            if (FromModifiedBy.HasValue)
-                and.Add(x => x.Modified >= FromModifiedBy.Value);
+            if (CreatedQueryParams.LessOrEquals.HasValue)
+                and.Add(x => x.Created <= CreatedQueryParams.LessOrEquals.Value);
+
+            if (CreatedQueryParams.MoreOrEquals.HasValue)
+                and.Add(x => x.Created >= CreatedQueryParams.MoreOrEquals.Value);
+
+            if (ModifiedQueryParams.LessOrEquals.HasValue)
+                and.Add(x => x.Modified <= ModifiedQueryParams.LessOrEquals.Value);
+
+            if (ModifiedQueryParams.MoreOrEquals.HasValue)
+                and.Add(x => x.Modified >= ModifiedQueryParams.MoreOrEquals.Value);
 
             return base.ToFilter().Add(and);
         }
